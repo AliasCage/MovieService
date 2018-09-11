560d2f3fa708174eba68b16600a2e7bcc9899c3b
aliasCage <g.triquivel@gmail.com>
2018-09-12 02:56:58 +0300

    Рефакторинг. Перенос логики на слой сервиса

--- src/main/java/ru/aliascage/movie_service/integration/TheMovieDbClient.java
+++ src/main/java/ru/aliascage/movie_service/integration/TheMovieDbClient.java
@@ -14,11 +14,6 @@ import ru.aliascage.movie_service.config.MovieConfig;
 import ru.aliascage.movie_service.model.*;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static org.springframework.util.StringUtils.isEmpty;
 
@@ -31,6 +26,9 @@ public class TheMovieDbClient {
     private static final String SEARCH_PERSON_PATH = "/search/person";
     private static final String GENRES_PATH = "/genre/movie/list";
     private static final String API_KEY = "api_key";
+    private static final String PAGE = "page";
+    private static final String SORT_BY = "sort_by";
+    private static final String QUERY = "query";
 
     @Autowired
     private MovieConfig config;
@@ -39,7 +37,6 @@ public class TheMovieDbClient {
     private RestTemplate restTemplate;
 
     public MovieDetails getMovieDetails(Integer movieId) {
-
         URI uri = buildUri(config.getUrl() + MOVIE_PATH + movieId);
         HttpEntity<MovieDetails> httpEntity = new HttpEntity<>(new MovieDetails());
         ResponseEntity<MovieDetails> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, MovieDetails.class);
@@ -54,7 +51,6 @@ public class TheMovieDbClient {
     }
 
     public MovieList getMovieList(MovieListRequest request) {
-
         URI uri = UriComponentsBuilder.fromUriString(config.getUrl() + MOVIES_PATH)
                 .queryParam(API_KEY, config.getApiKey())
                 .queryParams(buildParamsMap(request))
@@ -64,70 +60,29 @@ public class TheMovieDbClient {
         return responseEntity.getBody();
     }
 
-    private PersonShort findPerson(String name) {
+    public PersonList findPerson(String name) {
         URI uri = UriComponentsBuilder.fromUriString(config.getUrl() + SEARCH_PERSON_PATH)
                 .queryParam(API_KEY, config.getApiKey())
-                .queryParam("query", name)
+                .queryParam(QUERY, name)
                 .build().toUri();
         HttpEntity<PersonList> httpEntity = new HttpEntity<>(new PersonList());
         ResponseEntity<PersonList> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, PersonList.class);
-
-        List<PersonShort> personShortList = Optional.ofNullable(responseEntity.getBody())
-                .map(PersonList::getResults)
-                .orElse(Collections.emptyList());
-        if (personShortList.size() == 1) {
-            return personShortList.get(0);
-        }
-        String msg = String.format("Found %s persons with name: %s", personShortList.size(), name);
-        throw new IllegalStateException(msg);
+        return responseEntity.getBody();
     }
 
     private MultiValueMap<String, String> buildParamsMap(MovieListRequest request) {
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>(3);
-        map.add("page", request.getPage().toString());
+
+        map.add(PAGE, request.getPage().toString());
         if (!isEmpty(request.getSort())) {
-            map.add("sort_by", request.getPage().toString());
+            map.add(SORT_BY, request.getPage().toString());
         }
         String filter = request.getFilter();
         if (!isEmpty(filter)) {
             String[] params = filter.split("=");
-            String field = params[0];
-            String value = params[1];
-            switch (field) {
-                case "with_actor":
-                    addActor(map, value);
-                    break;
-                case "with_genres":
-                    addGenres(map, value);
-                    break;
-                default:
-                    map.add(field, value);
-            }
+            map.add(params[0], params[1]);
         }
         return map;
-    }
-
-    private void addGenres(MultiValueMap<String, String> map, String value) {
-        List<Genre> genres = getGenres().getGenres();
-        String[] needGenres = value.split(",");
-        List<Integer> genresIds = new ArrayList<>(needGenres.length);
-        for (String genreName : needGenres) {
-            Integer id = genres.stream()
-                    .filter(genre -> genre.getName().equalsIgnoreCase(genreName))
-                    .findFirst()
-                    .map(Genre::getId)
-                    .orElseThrow(() -> new RuntimeException(String.format("Not found genre: %s", genreName)));
-            genresIds.add(id);
-        }
-        String result = genresIds.stream()
-                .map(String::valueOf)
-                .collect(Collectors.joining(","));
-        map.add("with_genres", result);
-    }
-
-    private void addActor(MultiValueMap<String, String> map, String value) {
-        Integer id = findPerson(value).getId();
-        map.add("with_cast", String.valueOf(id));
     }
 
     private URI buildUri(String urlParts) {
--- src/main/java/ru/aliascage/movie_service/service/MovieServiceImpl.java
+++ src/main/java/ru/aliascage/movie_service/service/MovieServiceImpl.java
@@ -5,8 +5,24 @@ import org.springframework.stereotype.Component;
 import ru.aliascage.movie_service.integration.TheMovieDbClient;
 import ru.aliascage.movie_service.model.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
+import static org.springframework.util.StringUtils.isEmpty;
+
 @Component
 public class MovieServiceImpl implements MovieService {
+
+    private static final String ACTOR_REQUEST_FIELD = "with_actor";
+    private static final String GENRES_REQUEST_FIELD = "with_genres";
+    private static final String GENRE_SPLITTER = ",";
+    private static final String FILTER_SPLITTER = "=";
+    private static final String GENRES_FORMAT_STRING = "with_genres=%s";
+    private static final String ACTOR_FORMAT_STRING = "with_cast=%s";
 
     @Autowired
     private TheMovieDbClient client;
@@ -18,6 +34,7 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     public MovieList getMovieList(MovieListRequest request) {
+        convertFilterField(request);
         return client.getMovieList(request);
     }
 
@@ -26,11 +43,62 @@ public class MovieServiceImpl implements MovieService {
         return client.getGenres();
     }
 
-    public Genre getGenreByName(String genreName) {
-        GenreList genres = client.getGenres();
+    private void convertFilterField(MovieListRequest request) {
+        String filter = request.getFilter();
+        if (!isEmpty(filter)) {
+            String[] params = filter.split(FILTER_SPLITTER);
+            String field = params[0];
+            String value = params[1];
+
+            switch (field) {
+                case ACTOR_REQUEST_FIELD:
+                    request.setFilter(addActor(value));
+                    break;
+                case GENRES_REQUEST_FIELD:
+                    request.setFilter(addGenres(value));
+                    break;
+                default:
+            }
+        }
+    }
+
+    private PersonShort findPerson(String name) {
+        PersonList personList = client.findPerson(name);
+        List<PersonShort> personShortList = Optional.ofNullable(personList)
+                .map(PersonList::getResults)
+                .orElse(Collections.emptyList());
+        if (personShortList.size() == 1) {
+            return personShortList.get(0);
+        }
+        String msg = format("Found %s persons with name: %s", personShortList.size(), name);
+        throw new IllegalStateException(msg);
+    }
+
+    private String addGenres(String value) {
+        GenreList genreList = client.getGenres();
+
+        String[] needGenres = value.split(GENRE_SPLITTER);
+        List<Integer> genresIds = new ArrayList<>(needGenres.length);
+        for (String genreName : needGenres) {
+            Genre genre = getGenreByName(genreList, genreName);
+            genresIds.add(genre.getId());
+        }
+        String result = genresIds.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(","));
+
+        return format(GENRES_FORMAT_STRING, result);
+    }
+
+    private String addActor(String value) {
+        Integer personId = findPerson(value).getId();
+        return format(ACTOR_FORMAT_STRING, personId);
+    }
+
+    public Genre getGenreByName(GenreList genres, String genreName) {
         return genres.getGenres().stream()
                 .filter(genre -> genreName.equals(genre.getName()))
                 .findFirst()
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(() -> new RuntimeException(format("Not found genre: %s", genreName)));
     }
 }
